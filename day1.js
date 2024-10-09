c06bdf7d725f99e1d2c336138879a5bc92b11101
Muqadas-Iqbal-0 <muqadas.iqbal.dev@gmail.com>
2024-10-09 19:16:39 +0500

    by adding function put it into variable and then call this variable
    by adding function put it into variable and then call this variable to show output

--- day1.js
+++ day1.js
@@ -2,6 +2,8 @@
 // Write a function createHelloWorld. It should return a new function that always returns "Hello World".
 
 const createHelloWorld = () => {
-    return () => "Hello World";
+    return  () => "Hello World";
 
 }
+const greeting = createHelloWorld()
+console.log(greeting());
