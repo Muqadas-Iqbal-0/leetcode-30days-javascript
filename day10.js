8eb7e2483c0a93e511e1f3f790a6b53552711b1a
Muqadas-Iqbal-0 <muqadas.iqbal.dev@gmail.com>
2024-10-15 15:05:09 +0500

    leetcode day10 problem
    leetcode day10 problem solution

---
+++ day10.js
@@ -0,0 +1,19 @@
+
+// Given a function fn, return a new function that is identical to the original function except that it ensures fn is called at most once.
+
+// The first time the returned function is called, it should return the same result as fn.
+// Every subsequent time it is called, it should return undefined.
+
+const once = (fn) => {
+    let isCall = false;
+    return(...args)=>{
+        if(isCall){
+            return undefined
+        }
+        isCall = true;
+        return fn(...args)
+    }
+}
+const fn = (a,b,c)=>(a+b+c);
+let oncefn = once(fn);
+console.log(oncefn(3,4,5))
